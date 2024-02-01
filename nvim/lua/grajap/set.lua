1e70fe01c2e7f0fb99efbd640a68f9974a49a308
GrajAP <102023887+GrajAP@users.noreply.github.com>
2024-02-01 14:16:07 +0100

    bar rework

--- nvim/lua/grajap/set.lua
+++ nvim/lua/grajap/set.lua
@@ -17,12 +17,12 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true 
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 999
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
