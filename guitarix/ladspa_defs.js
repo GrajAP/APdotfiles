9e25503d8022bb8bffd2111c97a2f5347c246ad0
GrajAP <102023887+GrajAP@users.noreply.github.com>
2024-02-01 14:51:14 +0100

    ok

--- guitarix/ladspa_defs.js
+++
@@ -1,5 +0,0 @@
-[
-  ["http://guitarix.sourceforge.net/plugins/gx_ampegsvt_#_ampegsvt_", -1, 0, "GxSVT"],
-  ["http://guitarix.sourceforge.net/plugins/gxautowah#wah", -1, 0, "GxWah"],
-  ["http://guitarix.sourceforge.net/plugins/gx_colwah_#_colwah_", -1, 0, "GxWahwah"]
-]
